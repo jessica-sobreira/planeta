b8793d93b628484f958da13cd18a8bef4ab73aa7
jessica-sobreira <j.jecristina@gmail.com>
2024-02-13 21:38:52 -0300

    root

--- src/Root.tsx
+++ src/Root.tsx
@@ -1,14 +1,49 @@
 import { Header } from "./components/Header";
-import { Container } from "@mui/material";
+import { Container, Grid } from "@mui/material";
 import { CardAtracao } from "./components/CardAtracao";
+import { Atracao } from "./models/atracao.model";
 
 
 export function Root() {
+
+    const fresno: Atracao = {
+        id: "1",
+        nome: "Fresno",
+        descricao: "Fresno",
+        horario: "18:00",
+        curtidas: 0,
+        descurtidas: 0,
+        urlFoto: "https://s3.wasabisys.com/images.planetaatlantida.com.br/2024/lineup/home/polaroid-fresno.png"
+    }
+
+    return (
     <>
     <Header />
         <Container>
-            <h1>Teste</h1>
-        <CardAtracao />
+            <h1>Line up</h1>
+
+            <Grid container>
+
+                <Grid item xs={12} sm={4} md={3} lg={3} xl={1}>
+                    <CardAtracao atracao={fresno} />
+                </Grid>
+
+                <Grid item xs={12} sm={4} md={3} lg={3} xl={1}>
+                    <CardAtracao atracao={fresno} />
+                </Grid>
+
+                <Grid item xs={12} sm={4} md={3} lg={3} xl={1}>
+                    <CardAtracao atracao={fresno} />
+                </Grid>
+
+                <Grid item xs={12} sm={4} md={3} lg={3} xl={1}>
+                    <CardAtracao atracao={fresno} />
+                </Grid>
+                
+            </Grid>
     </Container>
+        
     </>
+    )
+
 }
